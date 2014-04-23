035819f3e4d7cfdbfe4b9229b6600a4801dc42f7
ikarus-exe <x_dothacker_x@yahoo.com>
2014-04-23 16:16:15 -0700

    turning in monsters.rb homework

--- monsters.rb
+++ monsters.rb
@@ -42,13 +42,19 @@ puts $monsters.count{ |m| m[:legs] == 2 }
 #puts $monsters.select{|m| m[:legs]== 2}.map{|m| m[:name]}
 
 puts "How many monsters are nocturnal?"
-puts # your answer here
+  puts $monsters.count{ |m| m[:nocturnal] == true }
 puts "What are the names of the monsters that are nocturnal?"
-puts # your answer here
+  puts $monsters.select{ |m| m[:nocturnal] == true }.map{|m| m[:name]}
 puts "How many legs do all our monsters have?"
-puts # your answer here
+  puts $monsters.map{ |m| m[:legs] }.inject(:+)
 puts "What are the 2 most common dangers of our monsters?"
-puts # your answer here
+  count = Hash.new(0)
+  $monsters.map{ |m| m[:dangers] }.flatten.each{ |d| count[d] += 1}
+  count.each{ |k,v| puts k if v == count.values.max }
+  count.each{ |k,v| puts k if v == count.values.max-1 }
 puts "What are the 2 most common vulnerabilities of our monsters?"
-puts # your answer here
+  count = Hash.new(0)
+  $monsters.map{ |m| m[:vulnerabilities] }.flatten.each{ |d| count[d] += 1}
+  count.each{ |k,v| puts k if v == count.values.max }
+  count.each{ |k,v| puts k if v == count.values.max-1 }
 
